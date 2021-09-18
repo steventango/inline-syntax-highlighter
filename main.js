e3a98b16c666ddf5c6706b4ebf06791a8c79264b
Steven Tang <18170455+steventango@users.noreply.github.com>
2021-09-18 00:23:31 -0600

    Refactor

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const preview_code = document.querySelector('pre code');
 const url = new URL(window.location.href);
 const properties = ['background', 'background-color', 'color', 'font-weight', 'font-style'];
 
+const link = document.querySelector('link');
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 const language_selector = document.getElementById('language_selector');
@@ -30,16 +31,21 @@ function change_language() {
   localStorage.setItem('language', language);
   language_selector.value = language;
   history.pushState(null, '', url);
-  hljs.highlightElement(preview_code);
+  update();
 }
 
 function change_theme() {
-  document.querySelector('link').href = `https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/${theme}.min.css`;
+  link.href = `https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/${theme}.min.css`;
+  link.addEventListener('load', update);
+  // Chrome doesn't support fire the load event for some reason
+  if (window.navigator.userAgent.includes('Chrome')) {
+    window.setTimeout(update, 1000);
+  }
   url.searchParams.set('theme', theme);
   theme_selector.value = theme;
   localStorage.setItem('theme', theme);
   history.pushState(null, '', url);
-  hljs.highlightElement(preview_code);
+  update();
 }
 
 function toggle_letter_spacing() {
@@ -53,29 +59,36 @@ function toggle_letter_spacing() {
     localStorage.setItem('letter_spacing', false);
   }
   history.pushState(null, '', url);
-  inline();
-  output.value = preview.outerHTML;
+  update();
 }
 
 function inline_style(element) {
+  for (let property of properties) {
+    element.style[property] = '';
+  }
   const styles = window.getComputedStyle(element);
   for (let key in styles) {
     // https://stackoverflow.com/questions/25097566/css-style-to-inline-style-via-javascript
-    let prop = key.replace(/\-([a-z])/g, v => v[1].toUpperCase());
+    let property = key.replace(/\-([a-z])/g, v => v[1].toUpperCase());
     if (properties.indexOf(key) > -1) {
-      element.style[prop] = styles[key];
+      element.style[property] = styles[property];
     }
   }
 }
 
 function inline() {
   const nodes = document.querySelectorAll('pre *');
-  inline_style(preview);
   if (nodes) {
     Array.from(nodes).forEach(element => {
       inline_style(element);
     });
   }
+}
+
+function update() {
+  hljs.highlightElement(preview_code);
+  inline();
+  output.value = preview.outerHTML;
 }
 
 language_selector.addEventListener('change', (e) => {
@@ -88,14 +101,12 @@ theme_selector.addEventListener('change', (e) => {
   change_theme();
 });
 
-letter_spacing_button.addEventListener('click', (e) => {
+letter_spacing_button.addEventListener('click', () => {
   letter_spacing = !letter_spacing;
   toggle_letter_spacing();
 });
 
 input.addEventListener('input', () => {
   preview_code.textContent = input.value;
-  hljs.highlightElement(preview_code);
-  inline();
-  output.value = preview.outerHTML;
+  update();
 });
